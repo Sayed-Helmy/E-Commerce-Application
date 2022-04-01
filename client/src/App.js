8fe927890596289751be94beb4728d5394f9e395
Mohamed Mostafa <mrokam@gmail.com>
2022-04-01 23:55:04 +0200

    add routers base

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import Layout from "./components/layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <Layout>
-      <div className="App"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="expenses" element={<SigninPage />} />
+          <Route path="invoices" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+      ,
     </Layout>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
