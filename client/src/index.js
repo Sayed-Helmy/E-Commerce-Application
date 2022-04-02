746563e6c95f13d43f5ec73aff2b7b820e9b0e37
Mohamed Mostafa <mrokam@gmail.com>
2022-04-03 00:57:35 +0200

    fix react 18 conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
