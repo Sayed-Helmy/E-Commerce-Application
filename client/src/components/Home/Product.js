d5fb8504e6c4a896771e81af33738fc05ce6a761
Mohamed Mostafa <mrokam@gmail.com>
2022-04-04 00:17:43 +0200

    create product component

---
+++ client/src/components/Home/Product.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Product = ({ product }) => {
+  return (
+    <a key={product.id} href={product.href} className="group">
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <img
+          src={product.imageSrc}
+          alt={product.imageAlt}
+          className="w-full h-full object-center object-cover group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+    </a>
+  );
+};
+
+export default Product;
