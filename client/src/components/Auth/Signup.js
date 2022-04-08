3d09abe31cae6289f6ef83935bbb9d136bdfefb0
Mohamed Mostafa <mrokam@gmail.com>
2022-04-09 01:44:51 +0200

    minor typo in signup heading sec

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -27,7 +27,7 @@ const Signup = () => {
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign up a new account
+              Create a new account
             </h2>
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
