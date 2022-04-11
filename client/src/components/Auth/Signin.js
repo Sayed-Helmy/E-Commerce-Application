69cdb6169bd614a9df089049a627b1bba056336f
Mohamed Mostafa <mrokam@gmail.com>
2022-04-11 15:31:17 +0200

    adjust padding

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -23,7 +23,7 @@ const Signin = () => {
 
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-28 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-full flex items-center justify-center py-14 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
