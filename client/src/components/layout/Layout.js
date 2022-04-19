bdc126e6d320c36a84e577448108ba716b9d338f
Sayed Helmy <s.helmy50@gmail.com>
2022-04-19 17:16:33 +0200

    Price Filter Done - Admin Rout Full fix

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -5,7 +5,6 @@ import { useLocation } from "react-router-dom";
 
 const Layout = (props) => {
   const location = useLocation();
-  console.log(location);
   return (
     <>
       {!location.pathname.startsWith("/admin") && <MainNavigation />}
