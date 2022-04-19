35664257c44ff3b0b2a6b14cb65bc334fc31f6ac
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-04-19 06:56:51 +0200

    price filter, search-bar

--- client/src/components/Shop/shopFilter.js
+++ client/src/components/Shop/shopFilter.js
@@ -6,6 +6,7 @@ import ShopProducts from "./shopProducts";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { productsActions } from "../../store/productsSlice";
+import PriceFilter from "./priceFilter";
 
 const filters = [
   {
@@ -14,19 +15,19 @@ const filters = [
     opened: true,
     options: [],
   },
-  {
-    id: "price",
-    name: "Price",
-    opened: false,
-    options: [
-      { value: "2l", label: "2L", checked: false },
-      { value: "6l", label: "6L", checked: false },
-      { value: "12l", label: "12L", checked: false },
-      { value: "18l", label: "18L", checked: false },
-      { value: "20l", label: "20L", checked: false },
-      { value: "40l", label: "40L", checked: true },
-    ],
-  },
+  // {
+  //   id: "price",
+  //   name: "Price",
+  //   opened: false,
+  //   options: [
+  //     { value: "2l", label: "2L", checked: false },
+  //     { value: "6l", label: "6L", checked: false },
+  //     { value: "12l", label: "12L", checked: false },
+  //     { value: "18l", label: "18L", checked: false },
+  //     { value: "20l", label: "20L", checked: false },
+  //     { value: "40l", label: "40L", checked: true },
+  //   ],
+  // },
 ];
 
 export default function ShopFilter() {
@@ -43,10 +44,7 @@ export default function ShopFilter() {
     let url = `http://localhost:5000/api/v1/products`;
     if (e.target.checked) catArray = [...catArray, e.target.value];
     else catArray = catArray.filter((i) => i !== e.target.value);
-    if (catArray.length > 0)
-      url = `http://localhost:5000/api/v1/products?${
-        section.id
-      }=${catArray.join(",")}`;
+    if (catArray.length > 0) url = `http://localhost:5000/api/v1/products?${section.id}=${catArray.join(",")}`;
     const result = await axios.get(url, {
       withCredentials: true,
     });
@@ -57,40 +55,16 @@ export default function ShopFilter() {
       <div>
         {/* Mobile filter dialog */}
         <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="fixed inset-0 z-40 flex lg:hidden"
-            onClose={setMobileFiltersOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
+          <Dialog as="div" className="fixed inset-0 z-40 flex lg:hidden" onClose={setMobileFiltersOpen}>
+            <Transition.Child as={Fragment} enter="transition-opacity ease-linear duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity ease-linear duration-300" leaveFrom="opacity-100" leaveTo="opacity-0">
               <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
             </Transition.Child>
 
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
+            <Transition.Child as={Fragment} enter="transition ease-in-out duration-300 transform" enterFrom="translate-x-full" enterTo="translate-x-0" leave="transition ease-in-out duration-300 transform" leaveFrom="translate-x-0" leaveTo="translate-x-full">
               <div className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
                 <div className="flex items-center justify-between px-4">
                   <h2 className="text-lg font-medium text-gray-900">Filters</h2>
-                  <button
-                    type="button"
-                    className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                    onClick={() => setMobileFiltersOpen(false)}
-                  >
+                  <button type="button" className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400" onClick={() => setMobileFiltersOpen(false)}>
                     <span className="sr-only">Close menu</span>
                     <XIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
@@ -98,52 +72,21 @@ export default function ShopFilter() {
                 {/* Filters */}
                 <form className="mt-4 border-t text-black/70">
                   {filters.map((section) => (
-                    <Disclosure
-                      as="div"
-                      key={section.id}
-                      className="border-t border-gray-200 px-4 py-6"
-                    >
+                    <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
                       {({ open }) => (
                         <>
                           <h3 className="-mx-2 -my-3 flow-root">
                             <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                              <span className="font-medium text-gray-900">
-                                {section.name}
-                              </span>
-                              <span className="ml-6 flex items-center">
-                                {open ? (
-                                  <MinusSmIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                ) : (
-                                  <PlusSmIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                )}
-                              </span>
+                              <span className="font-medium text-gray-900">{section.name}</span>
+                              <span className="ml-6 flex items-center">{open ? <MinusSmIcon className="h-5 w-5" aria-hidden="true" /> : <PlusSmIcon className="h-5 w-5" aria-hidden="true" />}</span>
                             </Disclosure.Button>
                           </h3>
                           <Disclosure.Panel className="pt-6">
                             <div className="space-y-6">
                               {section.options.map((option, optionIdx) => (
-                                <div
-                                  key={option.value}
-                                  className="flex items-center"
-                                >
-                                  <input
-                                    id={`filter-mobile-${section.id}-${optionIdx}`}
-                                    name={`${section.id}[]`}
-                                    defaultValue={option.value}
-                                    type="checkbox"
-                                    defaultChecked={option.checked}
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                  />
-                                  <label
-                                    htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                                    className="ml-3 min-w-0 flex-1 text-gray-500"
-                                  >
+                                <div key={option.value} className="flex items-center">
+                                  <input id={`filter-mobile-${section.id}-${optionIdx}`} name={`${section.id}[]`} defaultValue={option.value} type="checkbox" defaultChecked={option.checked} className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                                  <label htmlFor={`filter-mobile-${section.id}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
                                     {option.label}
                                   </label>
                                 </div>
@@ -160,10 +103,8 @@ export default function ShopFilter() {
           </Dialog>
         </Transition.Root>
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="relative z-10 flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
-            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">
-              Shop
-            </h1>
+          <div className="  flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">Shop</h1>
           </div>
 
           <section aria-labelledby="products-heading" className="pt-6 pb-24">
@@ -173,44 +114,22 @@ export default function ShopFilter() {
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
               {/* Filters */}
-              <form className="hidden lg:block">
+              <div className="hidden lg:block">
                 {categories &&
                   filters.map((section) => (
-                    <Disclosure
-                      defaultOpen={section.opened}
-                      as="div"
-                      key={section.id}
-                      className="border-b border-gray-200 py-6"
-                    >
+                    <Disclosure defaultOpen={section.opened} as="div" key={section.id} className="border-b border-gray-200 py-6">
                       {({ open }) => (
                         <>
                           <h3 className="-my-3 flow-root">
                             <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                              <span className="font-medium text-gray-900">
-                                {section.name}
-                              </span>
-                              <span className="ml-6 flex items-center">
-                                {open ? (
-                                  <MinusSmIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                ) : (
-                                  <PlusSmIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                )}
-                              </span>
+                              <span className="font-medium text-gray-900">{section.name}</span>
+                              <span className="ml-6 flex items-center">{open ? <MinusSmIcon className="h-5 w-5" aria-hidden="true" /> : <PlusSmIcon className="h-5 w-5" aria-hidden="true" />}</span>
                             </Disclosure.Button>
                           </h3>
                           <Disclosure.Panel className="pt-6">
                             <div className="space-y-4">
                               {section.options.map((option, optionIdx) => (
-                                <div
-                                  key={option.value}
-                                  className="flex items-center"
-                                >
+                                <div key={option.value} className="flex items-center">
                                   <input
                                     id={`filter-${section.id}-${optionIdx}`}
                                     name={`${section.id}[]`}
@@ -220,10 +139,7 @@ export default function ShopFilter() {
                                     onChange={(e) => checkedHandler(e, section)}
                                     className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                   />
-                                  <label
-                                    htmlFor={`filter-${section.id}-${optionIdx}`}
-                                    className="ml-3 cursor-pointer text-sm text-gray-600"
-                                  >
+                                  <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 cursor-pointer text-sm text-gray-600">
                                     {option.label}
                                   </label>
                                 </div>
@@ -234,7 +150,14 @@ export default function ShopFilter() {
                       )}
                     </Disclosure>
                   ))}
-              </form>
+                <form>
+                  <h3 className="my-10 font-medium text-gray-900">Price</h3>
+                  <PriceFilter min={0} max={1000} onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)} />
+                  <button type="submit" Value="" className="mx-auto mt-16 block rounded-md bg-black px-4 text-white">
+                    Filter
+                  </button>
+                </form>
+              </div>
 
               {/* Product grid */}
               <div className="lg:col-span-3">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,8 +7,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms"), require("autoprefixer")],
 };
