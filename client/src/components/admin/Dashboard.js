ac2a2c43e6ae1edf5a97318658dd3c5e24aa8ecf
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-20 14:21:01 +0200

    create table components and add it to dashboard

--- client/src/components/admin/Dashboard.js
+++ client/src/components/admin/Dashboard.js
@@ -1,11 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
+import Map from "./table/Map";
+import Table, {
+  SelectColumnFilter,
+  StatusPill,
+  LocateCell,
+  GlobalFilter,
+} from "./table/Table";
+import "./table/styles.css";
+import { MapProvider } from "./table/map-context";
 
-const Dashboard = () => {
-  return (
-    <div className="flex h-screen items-center justify-center bg-yellow-100">
-      Dashboard
-    </div>
+const getData = () => [
+  {
+    name: "Jane Cooper",
+    email: "jane.cooper@example.com",
+    title: "Regional Paradigm Technician",
+    department: "Optimization",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate1",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Cody Fisher",
+    email: "cody.fisher@example.com",
+    title: "Product Directives Officer",
+    department: "Intranet",
+    status: "Active",
+    role: "Owner",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Esther Howard",
+    email: "esther.howard@example.com",
+    title: "Forward Response Developer",
+    department: "Directives",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Jenny Wilson",
+    email: "jenny.wilson@example.com",
+    title: "Central Security Manager",
+    department: "Program",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Kristin Watson",
+    email: "kristin.watson@example.com",
+    title: "Lean Implementation Liaison",
+    department: "Mobility",
+    status: "Active",
+    role: "Admin",
+    locate: "Locate",
+    imgUrl:
+      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    name: "Cameron Williamson",
+    email: "cameron.williamson@example.com",
+    title: "Internal Applications Engineer",
+    department: "Security",
+    status: "Active",
+    role: "Member",
+    locate: "Locate",
+    posCoords: [260, 900],
+    imgUrl:
+      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+];
+
+export default function App() {
+  const [mapFly, setMapFly] = useState(null);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Locate",
+        accessor: "locate",
+        Cell: LocateCell,
+        posAccessor: "posCoords",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        Cell: StatusPill,
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+        Filter: GlobalFilter,
+        filter: "includes",
+      },
+    ],
+    []
   );
-};
 
-export default Dashboard;
+  const data = React.useMemo(() => getData(), []);
+
+  return (
+    <>
+      <div className="App" style={{ height: "100%" }}>
+        <div className="min-h-screen  text-gray-900">
+          <main className="container mx-auto px-4 pt-4 sm:px-6 lg:px-8">
+            <div className="">
+              <h1 className="text-xl font-semibold">Orders</h1>
+            </div>
+            <div className="mt-4">
+              <Table columns={columns} data={data} />
+              {/* <Table columns={columns} data={data} map={mapFly} /> */}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/admin/shared/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { classNames } from "./Utils";
+
+export function Button({ children, className, ...rest }) {
+  return (
+    <button
+      type="button"
+      className={classNames(
+        "relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+export function PageButton({ children, className, ...rest }) {
+  return (
+    <button
+      type="button"
+      className={classNames(
+        "relative inline-flex items-center border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
