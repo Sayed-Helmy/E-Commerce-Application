cff225b530d4ca2d50e61861f91e85bbdde99368
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-04-20 07:46:49 +0200

    change password, choose addresses

--- client/src/components/layout/MainNavigation.js
+++ client/src/components/layout/MainNavigation.js
@@ -41,19 +41,19 @@ export default function MainNavigation() {
           </Transition.Child>
 
           <Transition.Child as={Fragment} enter="transition ease-in-out duration-300 transform" enterFrom="-translate-x-full" enterTo="translate-x-0" leave="transition ease-in-out duration-300 transform" leaveFrom="translate-x-0" leaveTo="-translate-x-full">
-            <div className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
+            <div className="relative flex flex-col w-full max-w-xs pb-12 overflow-y-auto bg-white shadow-xl">
               <div className="flex px-4 pt-5 pb-2">
-                <button type="button" className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400" onClick={() => setOpen(false)}>
+                <button type="button" className="inline-flex items-center justify-center p-2 -m-2 text-gray-400 rounded-md" onClick={() => setOpen(false)}>
                   <span className="sr-only">Close menu</span>
-                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                  <XIcon className="w-6 h-6" aria-hidden="true" />
                 </button>
               </div>
 
-              <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+              <div className="px-4 py-6 space-y-6 border-t border-gray-200">
                 {/* Mobile menu links */}
                 {navigation.pages.map((page) => (
                   <div key={page.name} className="flow-root">
-                    <Link to={page.href} className="-m-2 block p-2 font-medium text-gray-900">
+                    <Link to={page.href} className="block p-2 -m-2 font-medium text-gray-900">
                       {page.name}
                     </Link>
                   </div>
@@ -61,14 +61,14 @@ export default function MainNavigation() {
               </div>
 
               {!user && (
-                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                <div className="px-4 py-6 space-y-6 border-t border-gray-200">
                   <div className="flow-root">
-                    <Link to="SigninPage" className="-m-2 block p-2 font-medium text-gray-900">
+                    <Link to="SigninPage" className="block p-2 -m-2 font-medium text-gray-900">
                       Sign in
                     </Link>
                   </div>
                   <div className="flow-root">
-                    <Link to="SignupPage" className="-m-2 block p-2 font-medium text-gray-900">
+                    <Link to="SignupPage" className="block p-2 -m-2 font-medium text-gray-900">
                       Create account
                     </Link>
                   </div>
@@ -80,26 +80,26 @@ export default function MainNavigation() {
       </Transition.Root>
 
       <header className="relative bg-white">
-        <p className="flex h-10 items-center justify-center bg-gray-900 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">Get free delivery on orders over $100</p>
+        <p className="flex items-center justify-center h-10 px-4 text-sm font-medium text-white bg-gray-900 sm:px-6 lg:px-8">Get free delivery on orders over $100</p>
 
-        <nav aria-label="Top" className="mx-auto max-w-2xl px-4 sm:px-6 md:max-w-7xl lg:px-8">
+        <nav aria-label="Top" className="max-w-2xl px-4 mx-auto sm:px-6 md:max-w-7xl lg:px-8">
           <div className="border-b border-gray-200">
-            <div className="flex h-16 items-center ">
-              <button type="button" className="rounded-md bg-white p-2 text-gray-400 lg:hidden" onClick={() => setOpen(true)}>
+            <div className="flex items-center h-16 ">
+              <button type="button" className="p-2 text-gray-400 bg-white rounded-md lg:hidden" onClick={() => setOpen(true)}>
                 <span className="sr-only">Open menu</span>
-                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                <MenuIcon className="w-6 h-6" aria-hidden="true" />
               </button>
 
               {/* Logo */}
-              <div className="ml-4 flex lg:ml-0">
+              <div className="flex ml-4 lg:ml-0">
                 <Link to="/">
                   <span className="sr-only">Logo</span>
-                  <img className="h-8 w-auto" src="/assets/logo.svg" alt="" />
+                  <img className="w-auto h-8" src="/assets/logo.svg" alt="" />
                 </Link>
               </div>
 
               <div className="hidden lg:block">
-                <ul className="ml-5 flex items-center justify-center space-x-8 text-sm">
+                <ul className="flex items-center justify-center ml-5 space-x-8 text-sm">
                   <li>
                     <Link to="/shop" className="">
                       Shop
@@ -113,11 +113,11 @@ export default function MainNavigation() {
                 </ul>
               </div>
 
-              <div className="flex w-full items-center justify-end space-x-4">
+              <div className="flex items-center justify-end w-full space-x-4">
                 {/* Search */}
-                <form action="" className="absolute inset-x-1/4	top-32 w-auto  overflow-hidden rounded-lg border-2 sm:static sm:ml-6 sm:flex sm:flex-1 sm:flex-row-reverse sm:items-center">
-                  <SearchIcon className="mx-3 hidden h-5 w-5 sm:block" aria-hidden="true" />
-                  <input className="mx-3 border-0 py-1 outline-0 sm:w-full" type="" placeholder="What are looking for?" />
+                <form action="" className="absolute w-auto overflow-hidden border-2 rounded-lg inset-x-1/4 top-28 sm:static sm:ml-6 sm:flex sm:flex-1 sm:flex-row-reverse sm:items-center">
+                  <SearchIcon className="hidden w-5 h-5 mx-3 sm:block" aria-hidden="true" />
+                  <input className="py-1 mx-3 border-0 outline-0 sm:w-full" type="" placeholder="What are you looking for?" />
                   <span className="sr-only">Search</span>
                 </form>
 
@@ -126,7 +126,7 @@ export default function MainNavigation() {
                     <Link to="SigninPage" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                       Sign in
                     </Link>
-                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                    <span className="w-px h-6 bg-gray-200" aria-hidden="true" />
                     <Link to="SignupPage" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                       Create account
                     </Link>
@@ -140,8 +140,8 @@ export default function MainNavigation() {
                 {/* Cart */}
                 <div className="flow-root lg:ml-6" onClick={() => setCartOpen(true)}>
                   <SliderCart open={cartOpen} setOpen={setCartOpen} />
-                  <div className="group -m-2 flex cursor-pointer items-center p-2">
-                    <ShoppingBagIcon className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                  <div className="flex items-center p-2 -m-2 cursor-pointer group">
+                    <ShoppingBagIcon className="flex-shrink-0 w-6 h-6 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{cartCount}</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </div>
