b0cafeaef107f6202715c8c4852cc8020598e702
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-27 00:22:06 +0200

    small add

--- client/src/components/admin/Orders.js
+++ client/src/components/admin/Orders.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import OrdersModal from "./Orders/OrdersModal";
 import DataTableBase from "./table/DataTableBase";
@@ -33,6 +34,22 @@ const columns = [
     selector: (row) => row.createdDate,
     sortable: true,
   },
+  {
+    name: "Actions",
+    // selector: (row) => row.price,
+    cell: (row, index, column, id) => (
+      <>
+        <div className=" flex flex-row gap-2 text-xs font-semibold tracking-wider">
+          <div className="mr-2 rounded-lg bg-blue-200 px-4 py-2 ">
+            <Link to="/">update</Link>
+          </div>
+          <div className="mr-2 rounded-lg bg-red-200 px-4 py-2">
+            <Link to="/">delete</Link>
+          </div>
+        </div>
+      </>
+    ),
+  },
 ];
 
 const data = [
@@ -58,7 +75,13 @@ const data = [
 function Orders() {
   return (
     <div className=" mx-auto min-h-screen max-w-2xl px-4 py-8 sm:px-6 md:max-w-7xl lg:px-8">
-      <DataTableBase columns={columns} data={data} progressPending={false} selectableRows subHeaderComponent={<Title />} />
+      <DataTableBase
+        columns={columns}
+        data={data}
+        progressPending={false}
+        selectableRows
+        subHeaderComponent={<Title />}
+      />
       <OrdersModal />
     </div>
   );
