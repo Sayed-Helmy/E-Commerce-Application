b79a7be51fc1afe34fa060a4fb78b48515ccf481
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-27 12:40:00 +0200

    newCustomerChart

---
+++ client/src/components/admin/charts/NewCustomersChart.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "users",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#000",
+    },
+  ],
+};
+
+export function NewCustomersChart() {
+  return <Bar options={options} data={data} />;
+}
