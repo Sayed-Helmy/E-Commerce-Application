89fd0975d33a877e6e7d64e70c6aa9cb7a1339d2
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-04-28 01:57:40 +0200

    dashboard - UserPage edit

--- client/src/components/admin/user/UserInfo.js
+++ client/src/components/admin/user/UserInfo.js
@@ -10,131 +10,60 @@ const UserInfo = ({ user }) => {
     <div>
       <Tab.Group>
         <Tab.List className=" flex">
-          <Tab
-            className={({ selected }) =>
-              classNames(
-                "w-full py-2.5 px-1 text-base",
-                selected ? "rounded-lg bg-black text-white" : ""
-              )
-            }
-          >
-            Info
-          </Tab>
-          <Tab
-            className={({ selected }) =>
-              classNames(
-                "w-full whitespace-nowrap py-2.5 px-1	text-base",
-                selected ? "rounded-lg bg-black text-white" : ""
-              )
-            }
-          >
-            Set Password
-          </Tab>
+          <Tab className={({ selected }) => classNames("w-full py-2.5 px-1 text-base", selected ? "rounded-lg bg-black text-white" : "")}>Info</Tab>
+          <Tab className={({ selected }) => classNames("w-full whitespace-nowrap py-2.5 px-1	text-base", selected ? "rounded-lg bg-black text-white" : "")}>Set Password</Tab>
         </Tab.List>
         <Tab.Panels>
           <Tab.Panel>
             {/* Info */}
             <form className="mt-7 space-y-5">
               {/* name */}
-              <div>
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-medium text-gray-700"
-                >
+              <div className="flex items-center space-x-2 sm:space-x-4">
+                <label htmlFor="cat-name" className="w-24 whitespace-nowrap rounded-l-lg bg-black/60 py-1 px-3 text-white">
                   Name
                 </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="first-name"
-                  defaultValue={user?.name}
-                  autoComplete="given-name"
-                  className="mt-1 block w-full rounded-md border-gray-500 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm"
-                />
+                <p>{user.name}</p>
+              </div>
+              {/* _id */}
+              <div className="flex items-center space-x-2 sm:space-x-4">
+                <label htmlFor="cat-name" className="w-24 whitespace-nowrap rounded-l-lg bg-black/60 py-1 px-3 text-white">
+                  ID
+                </label>
+                <p>{user._id}</p>
               </div>
               {/* email */}
-              <div>
-                <label
-                  htmlFor="email-address"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
+              <div className="flex items-center space-x-2 sm:space-x-4">
+                <label htmlFor="cat-name" className="w-24 whitespace-nowrap rounded-l-lg bg-black/60 py-1 px-3 text-white">
+                  E-Mail
                 </label>
-                <input
-                  type="text"
-                  name="email-address"
-                  id="email-address"
-                  value={user ? user.email : ""}
-                  autoComplete="email"
-                  className="mt-1 block w-full rounded-md border-gray-500 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm"
-                />
+                <p>{user.email}</p>
               </div>
               {/* Address */}
-              <div>
-                <label
-                  htmlFor="email-address"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Default Address
+              <div className="flex items-center space-x-2 sm:space-x-4">
+                <label htmlFor="cat-name" className="w-24 whitespace-nowrap rounded-l-lg bg-black/60 py-1 px-3 text-white">
+                  Address
                 </label>
-                <input
-                  type="text"
-                  name="email-address"
-                  id="email-address"
-                  value={user ? user.address : ""}
-                  autoComplete="email"
-                  className="mt-1 block w-full rounded-md border-gray-500 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm"
-                />
+                <p></p>
               </div>
               {/* Mobile */}
-              <div>
-                <label
-                  htmlFor="email-address"
-                  className="block text-sm font-medium text-gray-700"
-                >
+              <div className="flex items-center space-x-2 sm:space-x-4">
+                <label htmlFor="cat-name" className="w-24 whitespace-nowrap rounded-l-lg bg-black/60 py-1 px-3 text-white">
                   Mobile
                 </label>
-                <input
-                  type="text"
-                  name="email-address"
-                  id="email-address"
-                  value={user ? user.mobile : ""}
-                  autoComplete="email"
-                  className="mt-1 block w-full rounded-md border-gray-500 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm"
-                />
+                <p></p>
               </div>
-              {/* change */}
-              <button
-                type="submit"
-                className="rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/90 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-              >
-                Change
-              </button>
             </form>
           </Tab.Panel>
           <Tab.Panel>
             {/* Change Pass */}
             <div>
-              <form className="mt-7 space-y-5">
-                <label
-                  htmlFor="city"
-                  className="block text-sm font-medium text-gray-700"
-                >
+              <form className="mt-7 flex flex-col items-center justify-center space-y-5 xl:items-start">
+                <label htmlFor="city" className="block text-sm font-medium text-gray-700">
                   New Password
                 </label>
-                <input
-                  type="password"
-                  name="newPassword"
-                  id="city"
-                  autoComplete="address-level2"
-                  defaultValue=""
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm"
-                />
+                <input type="password" name="newPassword" id="city" autoComplete="address-level2" defaultValue="" className="mt-1 block w-full max-w-sm rounded-md border-gray-300 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm" />
                 {/* change */}
-                <button
-                  type="submit"
-                  className="rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/90 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                >
+                <button type="submit" className="max-w-sm rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/90 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
                   Set
                 </button>
               </form>
