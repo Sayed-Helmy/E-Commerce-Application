b2a8b74460b5ef95d5de18c2eb87ce8e4a0be346
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-04-27 10:32:01 +0200

    order page - delivery on

--- client/src/components/Profile/ManageOrder/ManageOrder.js
+++ client/src/components/Profile/ManageOrder/ManageOrder.js
@@ -15,12 +15,8 @@ const ManageOrder = () => {
       {orders?.length > 0 ? (
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 md:max-w-7xl lg:px-8 ">
           <div className="item-start flex flex-col justify-start space-y-2 ">
-            <h1 className="text-3xl font-semibold leading-7 text-gray-800 lg:text-4xl  lg:leading-9">
-              Order Summary
-            </h1>
-            <p className="text-base font-medium leading-6 text-gray-600">
-              {new Date(orders[0]?.createdAt).toLocaleString()}
-            </p>
+            <h1 className="text-3xl font-semibold leading-7 text-gray-800 lg:text-4xl  lg:leading-9">Order Summary</h1>
+            <p className="text-base font-medium leading-6 text-gray-600">{new Date(orders[0]?.createdAt).toLocaleString()}</p>
           </div>
           <div className="jusitfy-center mt-10 flex w-full flex-col items-stretch  space-y-4 md:space-y-6 xl:flex-row xl:space-x-8 xl:space-y-0">
             <div className="flex w-full flex-col items-start justify-start space-y-4 md:space-y-6 xl:space-y-8">
@@ -32,53 +28,42 @@ const ManageOrder = () => {
               {/*product summary need state still */}
               <div className="flex w-full flex-col items-stretch justify-center space-y-4 md:flex-row md:space-y-0 md:space-x-6 xl:space-x-8">
                 <div className="flex w-full flex-col space-y-6 rounded bg-gray-50  px-4 py-6 md:p-6 xl:p-8   ">
-                  <h3 className="text-xl font-semibold leading-5 text-gray-800">
-                    Summary
-                  </h3>
+                  <h3 className="text-xl font-semibold leading-5 text-gray-800">Summary</h3>
                   <div className="flex w-full flex-col items-center justify-center space-y-4 border-b border-gray-200 pb-4">
                     <div className="flex w-full  justify-between">
-                      <p className="text-base leading-4 text-gray-800">
-                        Subtotal
-                      </p>
-                      <p className="text-base leading-4 text-gray-600">
-                        ${orders[0].priceSubtotal}
-                      </p>
+                      <p className="text-base leading-4 text-gray-800">Subtotal</p>
+                      <p className="text-base leading-4 text-gray-600">${orders[0].priceSubtotal}</p>
                     </div>
                     <div className="flex w-full items-center justify-between">
-                      <p className="text-base leading-4 text-gray-800">
-                        Discount
-                      </p>
+                      <p className="text-base leading-4 text-gray-800">Discount</p>
                       <p className="text-base leading-4 text-gray-600">
-                        -${orders[0].amountDiscount} (
-                        {(orders[0].totalPrice / orders[0].priceSubtotal) * 100}
+                        -${orders[0].amountDiscount} ({(orders[0].totalPrice / orders[0].priceSubtotal) * 100}
                         %)
                       </p>
                     </div>
                     <div className="flex w-full items-center justify-between">
-                      <p className="text-base leading-4 text-gray-800">
-                        Shipping
-                      </p>
+                      <p className="text-base leading-4 text-gray-800">Shipping</p>
                       <p className="text-base leading-4 text-gray-600">Free</p>
                     </div>
                   </div>
                   <div className="flex w-full items-center justify-between">
-                    <p className="text-base font-semibold leading-4 text-gray-800">
-                      Total
-                    </p>
-                    <p className="text-base font-semibold leading-4 text-gray-600">
-                      ${orders[0].totalPrice}
-                    </p>
+                    <p className="text-base font-semibold leading-4 text-gray-800">Total</p>
+                    <p className="text-base font-semibold leading-4 text-gray-600">${orders[0].totalPrice}</p>
                   </div>
                 </div>
               </div>
             </div>
             <UserDate user={user} order={orders[0]} />
           </div>
-          <div className="mt-8 flex w-full items-center justify-center rounded bg-gray-50 px-4 py-6 md:justify-start">
-            <h3 className="mr-6 text-xl font-semibold leading-5 text-gray-800">
-              Order status:
-            </h3>
-            <p className="rounded-lg bg-green-300 px-3">{orders[0].status}</p>
+          <div className="mt-8 flex w-full flex-col space-y-4 rounded bg-gray-50 px-4 py-6 md:justify-start">
+            <div className="flex">
+              <h3 className="mr-6 text-xl font-semibold leading-5 text-gray-800">Order status:</h3>
+              <p className="rounded-lg bg-green-300 px-3">{orders[0].status}</p>
+            </div>
+            <div className="flex">
+              <h3 className="mr-6 text-xl font-semibold leading-5 text-gray-800">Delivery on:</h3>
+              <p className=""> 12/12/2022</p>
+            </div>
           </div>
         </div>
       ) : (
