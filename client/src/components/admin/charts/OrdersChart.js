9de218a1cf619be2604322e86d7e36ce74efaf1b
Mohamed Mostafa <mohamedhadia611@gmail.com>
2022-04-27 12:39:01 +0200

    OrdersChart

---
+++ client/src/components/admin/charts/OrdersChart.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+  },
+};
+
+const labels = ["Orders data"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "pending",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#64748b",
+    },
+    {
+      label: "in review",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#000",
+    },
+    {
+      label: "- in progress",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#0ea5e9",
+    },
+    {
+      label: "on the way",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#eab308",
+    },
+    {
+      label: "delivered",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "#16a34a",
+    },
+  ],
+};
+
+export function OrdersChart() {
+  return <Bar options={options} data={data} />;
+}
