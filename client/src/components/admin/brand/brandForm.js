b9fa80975c21e2b850632437830b50996fad934e
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-05-06 01:01:41 +0200

    brands - routes + add new + table

--- client/src/components/admin/brand/brandForm.js
+++ client/src/components/admin/brand/brandForm.js
@@ -6,18 +6,18 @@ const CategoryForm = () => {
       e.preventDefault();
       const formData = new FormData(e.currentTarget);
       try {
-         const category = await axios.post("http://localhost:5000/api/v1/categories", formData, {
+         const brand = await axios.post("http://localhost:5000/api/v1/brands", formData, {
             withCredentials: true,
          });
-         console.log(category);
+         console.log(brand);
       } catch (error) {
          console.log(error.response.data);
       }
    };
    return (
       <div className="container mx-auto max-w-7xl py-10 sm:mt-12 ">
-         <h3 className="mb-7 text-3xl font-bold">Create Category</h3>
-         <form onSubmit={submitHandler} className="flex flex-col justify-center gap-4 space-y-5 rounded-2xl border-2 py-10 px-3 sm:px-10">
+         <h3 className="mb-7 text-3xl font-bold">Create Brand</h3>
+         <form onSubmit={submitHandler} className="flex flex-col gap-4 space-y-5 rounded-2xl border-2 py-10 px-3 sm:px-10">
             <div className="flex items-center space-x-2 sm:space-x-4">
                <label htmlFor="cat-name" className="w-20">
                   Name:
@@ -29,26 +29,6 @@ const CategoryForm = () => {
                   Description:
                </label>
                <textarea id="" name="description" rows="2" cols="20" className=" w-60  rounded-md focus:border-black/50 focus:ring-black/50"></textarea>
-            </div>
-            <div className="flex items-center space-x-2 sm:space-x-4">
-               <label htmlFor="cat-Image" className="w-20">
-                  Image:
-               </label>
-               <input type="file" id="cat-Image" name="image" className="w-60  rounded-md border border-gray-300 bg-white  text-sm font-medium  text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-black/50 focus:ring-offset-1" />
-            </div>
-            <div className="flex items-center justify-start space-x-2 sm:space-x-4">
-               <label htmlFor="cat-featured" className="w-20">
-                  Featured:
-               </label>
-               <div className="w-60">
-                  <input type="checkbox" value={true} id="cat-featured" name="featured" className=" ring-2" />
-               </div>
-            </div>
-            <div className="flex items-center space-x-2 sm:space-x-4">
-               <label htmlFor="cat-name" className="w-20">
-                  Order:
-               </label>
-               <input type="text" id="cat-name" name="order" className="mt-1 block w-60  rounded-md border-gray-500 shadow-sm focus:border-black/50 focus:ring-black/50 sm:text-sm" />
             </div>
 
             <div className="flex justify-end pt-5">
---
+++ client/src/pages/admin/brand/brandPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BrandDetails from "../../../components/admin/brand/brandDetails";
+
+const CategoryPage = () => {
+   return (
+      <div>
+         <BrandDetails />
+      </div>
+   );
+};
+
+export default CategoryPage;
