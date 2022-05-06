5a4f753ba6d45d9f26e5c8f23a60d86e67868f43
Ahmed Khairy <ahkhfa7149@gmail.com>
2022-05-07 00:08:32 +0200

    client, server - delete

--- client/server/routes/coupons.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createCoupon, getCoupon } = require("../controllers/coupons");
-const router = express.Router();
-
-router.route("/").post(createCoupon);
-router.route("/check/:key").get(getCoupon);
-
-module.exports = router;
